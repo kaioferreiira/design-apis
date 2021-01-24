61906d724e3c74eb5cab0ac73c637f242e154495
Kaio Ferreira <kaio.costa@ntconsult.com.br>
2021-01-24 15:07:17 -0300

    Adicionando OpenAPI

--- src/main/java/com/example/design/api/DesignApiApplication.java
+++ src/main/java/com/example/design/api/DesignApiApplication.java
@@ -2,12 +2,14 @@ package com.example.design.api;
 
 import com.example.design.api.business.dto.EmployeeDTO;
 import com.example.design.api.business.service.EmployeeServiceImpl;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class DesignApiApplication  implements CommandLineRunner {
 
 
